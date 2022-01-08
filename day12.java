0db737e6bbccafb729953338cdf48417cb0ba8a6
C <79934740+VirgoColada@users.noreply.github.com>
2022-01-07 23:21:24 -0600

    Add files via upload

---
+++ day12.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+import java.io.*; 
+import static java.lang.System.*; 
+public class day12 {
+    public static void main(String args[])  throws IOException  {
+      Scanner kb = new Scanner(System.in);
+            System.out.print("Type something...");
+            kb.nextLine();
+System.out.println("Yeet");
+
+    }
+}
